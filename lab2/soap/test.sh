3842552097039dabdff49f331d0a86801c2447f7
as51340 <andi8647@gmail.com>
2021-10-16 11:05:35 +0200

    Organization changed

--- lab2/soap/test.sh
+++ lab2/soap/test.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+echo "Packaging application..."
+mvn clean package
+echo
+echo
 echo "Sending request: request.xml"
 echo
-curl --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws | xmllint --format -
+curl -v --header "content-type: text/xml" -d @request.xml http://localhost:8080/ws | xmllint --format -
 echo
 echo
 echo "Sending request: requestbiggest.xml"
 echo
-curl --header "content-type: text/xml" -d @requestbiggest.xml http://localhost:8080/ws | xmllint --format -
+curl -v --header "content-type: text/xml" -d @requestbiggest.xml http://localhost:8080/ws | xmllint --format -
 echo
 echo
 echo "Sending request: requestcheapest.xml"
 echo
-curl --header "content-type: text/xml" -d @requestcheapest.xml http://localhost:8080/ws | xmllint --format -
+curl -v --header "content-type: text/xml" -d @requestcheapest.xml http://localhost:8080/ws | xmllint --format -
 echo
 echo
 echo "Trying to create new order: createorder.xml"
 echo
-curl --header "content-type: text/xml" -d @createorder.xml http://localhost:8080/ws | xmllint --format -
+curl -v --header "content-type: text/xml" -d @createorder.xml http://localhost:8080/ws | xmllint --format -
