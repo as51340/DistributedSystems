bfed52435b946560416d7e972cea2d0707122b65
as51340 <andi8647@gmail.com>
2021-10-26 02:04:35 +0200

    working

---
+++ lab3/gRPC/src/main/java/org/example/AlmaOnline/provided/client/AlmaOnlineClientAdapter.java
@@ -0,0 +1,29 @@
+package org.example.AlmaOnline.provided.client;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.example.AlmaOnline.defaults.Script;
+import org.example.AlmaOnline.server.AlmaOnlineGrpc;
+
+import java.util.List;
+
+/**
+ * We need to implement this interface. Called by provided.app package.
+ */
+public interface AlmaOnlineClientAdapter {
+    List<RestaurantInfo> getRestaurants(AlmaOnlineGrpc.AlmaOnlineBlockingStub stub);
+
+    MenuInfo getMenu(AlmaOnlineGrpc.AlmaOnlineBlockingStub stub, String restaurantId);
+
+    ListenableFuture<?> createDineInOrder(AlmaOnlineGrpc.AlmaOnlineFutureStub stub,
+    		DineInOrderQuote order);
+
+    ListenableFuture<?> createDeliveryOrder(AlmaOnlineGrpc.AlmaOnlineFutureStub stub, 
+    		DeliveryOrder order);
+
+    BaseOrderInfo getOrder(AlmaOnlineGrpc.AlmaOnlineBlockingStub stub,
+    		String restaurantId, String orderId);
+
+    default AppScript getScript() {
+        return new Script();
+    }
+}
